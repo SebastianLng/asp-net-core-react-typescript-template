fa51634b953ddc670e831acca7276ee01eeb2096
Sebastian Lang <admin@sebastian-lang.eu>
2019-11-06 17:20:22 +0100

    Add bootstrap version ported to Typescript

---
+++ AspNetCoreReactTypescript-Bootstrap/ClientApp/src/components/Counter.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+interface ICounterProps {
+
+}
+
+interface ICounterState {
+    currentCount: number;
+}
+
+export class Counter extends Component<ICounterProps, ICounterState>  {
+  static displayName = Counter.name;
+
+  constructor(props: Readonly<ICounterProps>) {
+    super(props);
+    this.state = { currentCount: 0 };
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  incrementCounter() {
+    this.setState({
+      currentCount: this.state.currentCount + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a React component.</p>
+
+        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+
+        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+      </div>
+    );
+  }
+}
